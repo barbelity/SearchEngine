0a1e6536749f21c69e3786375635be0d6e72b57a
barbelity <barbq3@gmail.com>
2015-12-07 17:39:48 +0200

    123

--- SearchEngine/Model/Indexer.cs
+++ SearchEngine/Model/Indexer.cs
@@ -8,16 +8,59 @@ namespace SearchEngine.Model
 {
     class Indexer : iIndexer
     {
+		private int docsToFile;
+		public int DocsToFile 
+		{
+			get { return docsToFile;}
+			set { docsToFile = value;}
+		}
+
+		private Dictionary<string, Positions> tempIndex;
+		private int docsCounter = 0;
+
+		public Indexer()
+		{
+			this.docsToFile = 10;
+			tempIndex = new Dictionary<string, Positions>();
+		}
 
         public void saveTerms(Dictionary<string, Positions> terms)
         {
-            throw new NotImplementedException();
 			//add the terms received to a temp dictionary
 			//calculate doc's info
 			//save doc info to docs dictionary on memory
 			//after X time, write all data to posting file
 			//keep dictionary on memory for indexing
+			if (docsCounter == docsToFile)
+			{
+				try
+				{
+					writeTermsToFile();
+					tempIndex.Clear();
+				}
+				catch(Exception e)
+				{
+					Console.WriteLine("Exception writing to file");
+					throw e;
+				}
+			}
+			else
+			{
+				foreach (KeyValuePair<string, Positions> t in terms)
+				{
+
+				}
+			}
+			
+
+
+			throw new NotImplementedException();
         }
+
+		private void writeTermsToFile()
+		{
+			throw new NotImplementedException();
+		}
 
         public void startIndexing()
         {
